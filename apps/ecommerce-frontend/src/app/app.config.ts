05765355008a4140d1d71a8900ab6a1cb2792986
Stanley Morales <abrahamx.carranza@gmail.com>
2026-02-04 16:47:21 +0100

    feat: frontend setup

--- apps/ecommerce-frontend/src/app/app.config.ts
+++ apps/ecommerce-frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,6 +7,7 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
